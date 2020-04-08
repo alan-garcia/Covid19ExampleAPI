9496b8500d24d577fbd5d9009e44598e334bd734
Alan García <alangarcianavarro@outlook.com>
2020-04-08 11:20:01 +0200

    Creación del modelo "Summary" y del servicio para obtener en la API la información del apartado "summary".

---
+++ Covid19ExampleAPI/Controllers/Covid19Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Covid19ExampleAPI.Models.Summary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Covid19ExampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Covid19Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<Summary>> GetSummary()
+        {
+            var apiService = new ApiService();
+            var summaryInfo = await apiService.GetAsync<Summary>(@"https://api.covid19api.com/summary");
+
+            return summaryInfo;
+        }
+    }
+}
---
+++ Covid19ExampleAPI/Services/ApiService.cs
@@ -0,0 +1,39 @@
+﻿using Covid19ExampleAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Covid19ExampleAPI
+{
+    public class ApiService
+    {
+        private readonly int _loginTimeOut;
+
+        public ApiService() {
+            _loginTimeOut = 20;
+        }
+
+        public async Task<T> GetAsync<T>(string urlApi) where T : class
+        {
+            T covidContentInfo = null;
+
+            using (var httpClient = new HttpClient() { Timeout = TimeSpan.FromSeconds(_loginTimeOut) })
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await httpClient.GetAsync(urlApi);
+                if (response.IsSuccessStatusCode)
+                {
+                    var httpContent = await response.Content.ReadAsStringAsync();
+                    covidContentInfo = JsonConvert.DeserializeObject<T>(httpContent, JsonConfig.GetJsonSerializerSettings());
+                }
+
+                return covidContentInfo;
+            }
+        }
+
+    }
+}
