e68aa2e9ebf45cffbf827c6ec040f13a3cde3491
Alan García <alangarcianavarro@outlook.com>
2020-04-21 17:02:48 +0200

    Refactor: En los inputs de los formularios de fechas inicializarlos a la fecha actual.

--- Example.Covid19.WebUI/wwwroot/js/site.js
+++ Example.Covid19.WebUI/wwwroot/js/site.js
@@ -1,46 +1,30 @@
 ﻿$(function () {
-  var date = new Date();
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var year = date.getFullYear();
+  ValidateDateInputForm("byCountryDateFrom");
+  ValidateDateInputForm("byCountryDateTo");
+  ValidateDateInputForm("byCountryLiveDateFrom");
+  ValidateDateInputForm("byCountryLiveDateTo");
+  ValidateDateInputForm("byCountryTotalDateFrom");
+  ValidateDateInputForm("byCountryTotalDateTo");
+  ValidateDateInputForm("liveByCountryAndStatusAfterDate_Date");
 
-  if (month < 10) month = "0" + month;
-  if (day < 10) day = "0" + day;
-
-  var today = year + "-" + month + "-" + day;
-
-  if (document.getElementById("byCountryDateFrom") !== null) {
-    var byCountryDateFrom = document.getElementById("byCountryDateFrom");
-    byCountryDateFrom.value = today;
+  function ValidateDateInputForm(idNameInputDateForm) {
+    if (document.getElementById(idNameInputDateForm) !== null) {
+      var idNameInputDateForm_element = document.getElementById(idNameInputDateForm);
+      idNameInputDateForm_element.value = GetDateNow();
+    }
   }
 
-  if (document.getElementById("byCountryDateTo") !== null) {
-    var byCountryDateTo = document.getElementById("byCountryDateTo");
-    byCountryDateTo.value = today;
-  }
+  function GetDateNow() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
 
-  if (document.getElementById("byCountryLiveDateFrom") !== null) {
-    var byCountryLiveDateFrom = document.getElementById("byCountryLiveDateFrom");
-    byCountryLiveDateFrom.value = today;
-  }
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
 
-  if (document.getElementById("byCountryLiveDateTo") !== null) {
-    var byCountryLiveDateTo = document.getElementById("byCountryLiveDateTo");
-    byCountryLiveDateTo.value = today;
-  }
+    var today = year + "-" + month + "-" + day;
 
-  if (document.getElementById("byCountryTotalDateFrom") !== null) {
-    var byCountryTotalDateFrom = document.getElementById("byCountryTotalDateFrom");
-    byCountryTotalDateFrom.value = today;
-  }
-
-  if (document.getElementById("byCountryTotalDateTo") !== null) {
-    var byCountryTotalDateTo = document.getElementById("byCountryTotalDateTo");
-    byCountryTotalDateTo.value = today;
-  }
-
-  if (document.getElementById("liveByCountryAndStatusAfterDate_Date") !== null) {
-    var liveByCountryAndStatusAfterDate_Date = document.getElementById("liveByCountryAndStatusAfterDate_Date");
-    liveByCountryAndStatusAfterDate_Date.value = today;
+    return today;
   }
 });
